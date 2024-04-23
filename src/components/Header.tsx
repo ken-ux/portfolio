11318ba6a913c8e77d15164c19322123e3b809b5
Kenny Nguyen <sokenns@gmail.com>
2024-04-22 21:53:22 -0700

    Style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
+import { HomeIcon } from "@heroicons/react/24/solid";
+
 const Header = () => {
   return (
-    <header className="test">
-      <nav className="flex justify-between">
-        <a href="/">Name</a>
+    <header className="test sticky top-0 backdrop-blur-sm rounded-full px-6 py-2 m-auto w-4/5">
+      <nav className="flex justify-between items-center">
+        <a href="/">
+          <HomeIcon className="h-8 w-8" />
+        </a>
         <ul className="flex gap-8">
           <li>
-            <a href="#work">Work</a>
+            <a href="#projects">Projects</a>
           </li>
           <li>
             <a href="/about">About</a>
