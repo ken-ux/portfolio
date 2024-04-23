b4056832d4810abb99c305e53f5403b11d442f5a
Kenny Nguyen <sokenns@gmail.com>
2024-04-22 18:41:03 -0700

    Add buttons to projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,10 +2,16 @@ import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
 
 const Project = () => {
   const links: string[] = ["Github Repo", "Live"];
-  const listItems: JSX.Element[] = links.map((link) => {
+  const tags: string[] = ["HTML", "CSS", "JavaScript", "React", "MongoDB"];
+
+  const tagButtons: JSX.Element[] = tags.map((tag) => {
+    return <p>{tag}</p>;
+  });
+
+  const linksList: JSX.Element[] = links.map((link) => {
     return (
       <li className="self-start">
-        <a href="" className="test flex items-center">
+        <a href="" className="flex items-center">
           {link}&nbsp;
           <ArrowTopRightOnSquareIcon className="icon" />
         </a>
@@ -17,7 +23,8 @@ const Project = () => {
     <div className="test">
       <img src="" alt="" className="w-full h-64" />
       <h1>Project Title</h1>
-      <ul className="flex flex-col">{listItems}</ul>
+      <ul className="flex flex-col sm:flex-row sm:gap-4">{linksList}</ul>
+      <div className="flex flex-wrap gap-x-2">{tagButtons}</div>
     </div>
   );
 };
