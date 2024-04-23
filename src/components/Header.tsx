72085109f5a3a28057bc0d1007ffe80ee22c5375
Kenny Nguyen <sokenns@gmail.com>
2024-04-23 12:53:53 -0700

    Remove test border

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
-    <header className="test sticky top-0 backdrop-blur-sm rounded-full px-6 py-2 m-auto w-full md:w-4/5">
+    <header className="sticky top-0 rounded-full px-6 py-2 m-auto w-full md:w-4/5 border-2 border-zinc-200 backdrop-blur-sm bg-zinc-200/50">
       <nav className="flex justify-between items-center">
         <a href="/">
           <HomeIcon className="h-8 w-8" />
