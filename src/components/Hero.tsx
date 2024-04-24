2b6aa11cd509da3189da7601242e9cf2e35d42d6
Kenny Nguyen <sokenns@gmail.com>
2024-04-23 19:19:33 -0700

    Add styles and links to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,16 @@
 const Hero = () => {
   return (
-    <div>
-      <h1>Hi, I'm Kenny Nguyen.</h1>
-      <p>A full-stack software engineer based in Seattle.</p>
+    <div className="font-display my-12 text-5xl md:w-4/5">
+      <h1 className="inline">Hi, I'm Kenny Nguyen.</h1>
+      <span>
+        &nbsp;A full-stack software developer building applications that
+        highlight intuitive, approachable design.
+      </span>
+      <div className="mt-8 flex gap-4 text-3xl">
+        <a href="">GitHub</a>
+        <a href="">LinkedIn</a>
+        <a href="">Email</a>
+      </div>
     </div>
   );
 };
