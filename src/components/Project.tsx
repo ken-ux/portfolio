11bdbbcbc0066c8f0a65c4fb1a481ef5a1581f42
Kenny Nguyen <sokenns@gmail.com>
2024-04-24 02:56:54 -0700

    Add hover state to project images

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
+import url from "../assets/project_thumbnails/members_only.png";
 
 const Project = () => {
+  const [hover, setHover] = useState(false);
   const links: string[] = ["Github Repo", "Live"];
   const tags: string[] = ["HTML", "CSS", "JavaScript", "React", "MongoDB"];
 
@@ -20,9 +23,28 @@ const Project = () => {
   });
 
   return (
-    <div className="test">
-      <img src="" alt="" className="h-64 w-full" />
-      <h1>Project Title</h1>
+    <div>
+      {/* <div className="flex items-center justify-center">
+        <ul className="absolute flex flex-col sm:flex-row sm:gap-4">
+          {linksList}
+        </ul>
+        <img
+          src={url}
+          alt=""
+          className="w-full rounded-md border border-zinc-200 shadow-md"
+        />
+      </div> */}
+      <img
+        src={url}
+        alt=""
+        className={
+          "w-full rounded-md border border-zinc-200 shadow-md transition-all duration-200 ease-linear" +
+          (hover ? " opacity-70" : "")
+        }
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      />
+      <h3 className="mt-2 font-semibold">Project Title</h3>
       <ul className="flex flex-col sm:flex-row sm:gap-4">{linksList}</ul>
       <div className="flex flex-wrap gap-x-2">{tagButtons}</div>
     </div>
