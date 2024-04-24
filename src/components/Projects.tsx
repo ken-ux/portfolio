fbbb2bace2ac30312386857ddca1e137577a8b90
Kenny Nguyen <sokenns@gmail.com>
2024-04-24 02:21:01 -0700

    Style heading for project section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,8 +2,13 @@ import Project from "./Project";
 
 const Projects = () => {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="flex flex-col gap-4">
+      <h2
+        className="text-sm uppercase tracking-widest text-zinc-500"
+        id="projects"
+      >
+        Projects
+      </h2>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 lg:grid-cols-5">
         <Project />
         <Project />
