10ceb63e846342f56f5c30de9a9b3aebc8e1afcc
Kenny Nguyen <sokenns@gmail.com>
2024-04-23 19:27:06 -0700

    Change margin and increase line height

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,12 @@
 const Hero = () => {
   return (
-    <div className="font-display my-12 text-5xl md:w-4/5">
+    <div className="font-display mt-12 text-5xl leading-tight md:w-4/5">
       <h1 className="inline">Hi, I'm Kenny Nguyen.</h1>
       <span>
         &nbsp;A full-stack software developer building applications that
         highlight intuitive, approachable design.
       </span>
-      <div className="mt-8 flex gap-4 text-3xl">
+      <div className="mt-12 flex gap-4 text-3xl">
         <a href="">GitHub</a>
         <a href="">LinkedIn</a>
         <a href="">Email</a>
