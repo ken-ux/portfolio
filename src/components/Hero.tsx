d837c4cf8672a52a9fa985a3e8d67b6ef75e772a
Kenny Nguyen <sokenns@gmail.com>
2024-04-23 23:17:01 -0700

    Tighten letter spacing on heading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,15 @@
 const Hero = () => {
   return (
-    <div className="font-display mt-12 text-5xl leading-tight md:w-4/5">
-      <h1 className="inline">Hi, I'm Kenny Nguyen.</h1>
-      <span>
-        &nbsp;A full-stack software developer building applications that
-        highlight intuitive, approachable design.
-      </span>
-      <div className="mt-12 flex gap-4 text-3xl">
+    <div className="mt-12 md:w-4/5">
+      <div className="font-display text-5xl leading-tight tracking-tight">
+        <h1 className="inline">Hi, I'm Kenny Nguyen.</h1>
+        <span>
+          &nbsp;A full-stack software developer building applications that
+          highlight intuitive, approachable design.
+        </span>
+      </div>
+
+      <div className="mt-12 flex gap-4 text-2xl">
         <a href="">GitHub</a>
         <a href="">LinkedIn</a>
         <a href="">Email</a>
