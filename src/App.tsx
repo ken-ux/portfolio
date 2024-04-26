f9bbedac95ad8de01564de712337f348e58c09dd
Kenny Nguyen <sokenns@gmail.com>
2024-04-25 18:06:24 -0700

    Add commented code for using Outlet component instead of params

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+// import { Outlet } from "react-router-dom";
 
 function App() {
   const { name } = useParams();
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="flex flex-col gap-8">
       <Header />
+      {/* <Outlet /> */}
       {name === undefined && <Hero />}
       {name === "projects" && <Projects />}
       <hr />
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,16 @@
 import App from "./App.tsx";
+// import Hero from "./components/Hero.tsx";
+// import Projects from "./components/Projects.tsx";
 // import ErrorPage from "./ErrorPage";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    // children: [
+    //   { path: "/", element: <Hero /> },
+    //   { path: "projects", element: <Projects /> },
+    // ],
     // errorElement: <ErrorPage />,
   },
   {
