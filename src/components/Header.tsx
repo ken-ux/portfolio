f1012ff6746ba0510a27b1564399025adb63d18d
Kenny Nguyen <sokenns@gmail.com>
2024-04-26 02:43:44 -0700

    Increase gap between header links and change text color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,21 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 m-auto w-full rounded-full border-2 border-zinc-200 bg-zinc-200/50 px-6 py-2 backdrop-blur-sm md:w-4/5">
+    <header className="sticky top-0 m-auto w-full rounded-full border-2 border-zinc-200 bg-zinc-200/50 px-6 py-2 text-zinc-500 backdrop-blur-sm md:w-4/5">
       <nav className="flex items-center justify-between">
         <Link to="/">
           <HomeIcon className="h-8 w-8" />
         </Link>
-        <ul className="flex gap-4">
+        <ul className="flex gap-6">
           <li>
-            <Link to="/projects">Projects</Link>
+            <Link to="/projects" className="font-medium">
+              Projects
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" className="font-medium">
+              About
+            </Link>
           </li>
         </ul>
       </nav>
