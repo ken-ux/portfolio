c5546077936f559f83468c921ee5f64779edd7c8
Kenny Nguyen <sokenns@gmail.com>
2024-04-29 21:19:03 -0700

    Add custom animation to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
     fontFamily: {
       display: ["SF Pro Display", "system-ui", "sans-serif"],
     },
+    animation: {
+      fadein: "fadein 0.4s ease-in",
+    },
+    keyframes: {
+      fadein: {
+        from: { opacity: 0 },
+        to: { opacity: 1 },
+      },
+    },
   },
   plugins: [],
 };
