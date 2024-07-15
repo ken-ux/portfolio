4ca2b67b13e31f1bcf9160f86c6a8f3ad958bac7
Kenny Nguyen <sokenns@gmail.com>
2024-07-15 14:24:44 -0700

    Remove skills from home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,7 @@
 import Hero from "../components/Hero";
-import Skills from "../components/Skills";
 
 const HomePage = () => {
-  return (
-    <>
-      <Hero />
-      <hr />
-      <Skills />
-    </>
-  );
+  return <Hero />;
 };
 
 export default HomePage;
