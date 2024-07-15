b326e755b9e35c1a60d2ea6c9d7d52dd10ef6d1c
Kenny Nguyen <sokenns@gmail.com>
2024-07-15 14:25:38 -0700

    Make header full-width and only add auto margins horizontally

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
 import { HomeIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 m-auto w-full rounded-full border-2 border-zinc-200 bg-zinc-200/50 px-6 py-2 text-zinc-500 backdrop-blur-sm md:w-4/5">
+    <header className="sticky top-0 mx-auto w-full rounded-full border-2 border-zinc-200 bg-zinc-200/50 px-6 py-2 text-zinc-500 backdrop-blur-sm">
       <nav className="flex items-center justify-between">
         <Link to="/">
           <HomeIcon className="h-8 w-8" />
