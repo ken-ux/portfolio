298f49b1ceb1fbdfcf1ffcc0896973dbed59fb69
Kenny Nguyen <sokenns@gmail.com>
2024-07-16 13:31:57 -0700

    Lighten background and replace border with offset outline

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 mx-auto w-full rounded-full border-2 border-zinc-200 bg-zinc-200/50 px-6 py-2 text-zinc-500 backdrop-blur-sm">
+    <header className="sticky top-0 mx-auto w-full rounded-full bg-zinc-200/40 px-6 py-2 text-zinc-500 outline outline-offset-4 outline-zinc-200 backdrop-blur-sm">
       <nav className="flex items-center justify-between">
         <Link to="/">
           <HomeIcon className="h-8 w-8" />
