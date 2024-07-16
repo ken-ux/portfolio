22cc089485962f214654146c29404ee9ba93107f
Kenny Nguyen <sokenns@gmail.com>
2024-07-16 13:32:10 -0700

    Change text color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <footer className="flex justify-end">
-      <p>Kenny Nguyen © {new Date().getFullYear()}</p>
+      <p className="text-zinc-400">
+        Kenny Nguyen © {new Date().getFullYear()}
+      </p>
     </footer>
   );
 };
